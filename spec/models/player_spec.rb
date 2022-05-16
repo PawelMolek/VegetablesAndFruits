c3c8ce6a6418558f31e624652ebb91f45085b032
PawelMolek <pawel.molek@gmail.com>
2022-05-16 18:30:18 +0200

    Snack specs

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -5,43 +5,43 @@ RSpec.describe Player, :type => :model do
   context 'validation tests' do
 
     it "has correct name, point and active values" do
-      participant = Player.new(name: "name", points: 0, active: true).save
-      expect(participant).to eq(true)
+      player = Player.new(name: "name", points: 0, active: true).save
+      expect(player).to eq(true)
     end
 
     it "has too short name" do
-      participant = Player.new(name: "na", points: 0, active: true).save
-      expect(participant).to eq(false)
+      player = Player.new(name: "na", points: 0, active: true).save
+      expect(player).to eq(false)
     end
 
     it "has too long name" do
-      participant = Player.new(name: "participantparticipantparticipant", points: 0, active: true).save
-      expect(participant).to eq(false)
+      player = Player.new(name: "Player Player Player Player Player", points: 0, active: true).save
+      expect(player).to eq(false)
     end
 
     it "has nil value of points" do
-      participant = Player.new(name: "name", points: nil, active: true).save
-      expect(participant).to eq(false)
+      player = Player.new(name: "name", points: nil, active: true).save
+      expect(player).to eq(false)
     end
 
     it "has nil value of active" do
-      participant = Player.new(name: "name", points: 0, active: nil).save
-      expect(participant).to eq(false)
+      player = Player.new(name: "name", points: 0, active: nil).save
+      expect(player).to eq(false)
     end
 
     it "has not included point value which should be 0 at defualt" do
-      participant = Player.new(name: "name", active: true).save
-      expect(participant).to eq(true)
+      player = Player.new(name: "name", active: true).save
+      expect(player).to eq(true)
     end
 
     it "has not included active value which should be true at default" do
-      participant = Player.new(name: "name", points: 0).save
-      expect(participant).to eq(true)
+      player = Player.new(name: "name", points: 0).save
+      expect(player).to eq(true)
     end
 
     it "has not included active and points value which should be set by default" do
-      participant = Player.new(name: "name").save
-      expect(participant).to eq(true)
+      player = Player.new(name: "name").save
+      expect(player).to eq(true)
     end
   end
 end
---
+++ spec/models/snack_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Snack, :type => :model do
+
+  context 'validation tests' do
+
+    it "has correct name, point and active values" do
+      snack = Snack.new(name: "name", points: 0, active: true).save
+      expect(snack).to eq(true)
+    end
+
+    it "has too short name" do
+      snack = Snack.new(name: "na", points: 0, active: true).save
+      expect(snack).to eq(false)
+    end
+
+    it "has too long name" do
+      snack = Snack.new(name: "Snack Snack Snack SNack Snack Snack", points: 0, active: true).save
+      expect(snack).to eq(false)
+    end
+
+    it "has nil value of points" do
+      snack = Snack.new(name: "name", points: nil, active: true).save
+      expect(snack).to eq(false)
+    end
+
+    it "has nil value of active" do
+      snack = Snack.new(name: "name", points: 0, active: nil).save
+      expect(snack).to eq(false)
+    end
+
+    it "has not included point value which should be 0 at defualt" do
+      snack = Snack.new(name: "name", active: true).save
+      expect(snack).to eq(true)
+    end
+
+    it "has not included active value which should be true at default" do
+      snack = Snack.new(name: "name", points: 0).save
+      expect(snack).to eq(true)
+    end
+
+    it "has not included active and points value which should be set by default" do
+      snack = Snack.new(name: "name").save
+      expect(snack).to eq(true)
+    end
+  end
+end
