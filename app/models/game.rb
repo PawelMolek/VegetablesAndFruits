e1f6573d62b6b4e3a4b0f3d358da4f7ca0c05a6f
PawelMolek <pawel.molek@gmail.com>
2022-05-18 11:13:25 +0200

    Just want to save before modifications

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
 
   validates :game_date, comparison: { greater_than_or_equal_to: Date.today }
 
-  after_create :assign_snacks_to_players
+  # after_create :assign_snacks_to_players
 
   has_many :player_games
   has_many :players, through: :player_games
@@ -13,17 +13,17 @@ class Game < ApplicationRecord
   accepts_nested_attributes_for :snacks
 
   # assign to every players randomly a snacks included into game
-  def assign_snacks_to_players
+  # def assign_snacks_to_players
 
-    @snacks = Snack.all { |snack| snack if snack.active? }
-    @players = self.player_ids
-    length_of_game = @players.length
-    length_of_game.times do
-      @players.each do |player|
-        snack = @snacks.shuffle.first[:id]
-        SnackGame.create!(snack_id: snack, player_id: player, game_id: self.id)
-      end
-    end
-  end
+    #DEEP INTO CONTROLLER , LOOK AT DOCS OR STACKOVERFLOW HOW TO EDIT NESTED ATTRIBUTES
+
+    # @snacks = self.snack_ids
+    # snack = self.snack_ids
+    # @players = self.player_ids
+    # @players.each do |player|
+    #   snack = @snacks.shuffle.first
+      # SnackGame.create!(snack_id: snack, player_id: player, game_id: self.id)
+    # end
+  # end
 
 end
